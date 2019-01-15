936c2db4d10284406f140c9b99b107fccc7af7b2
Dyanna Tolman <dyannast@gmail.com>
2019-01-14 17:05:40 -0800

    rename test passing

--- test/chatroom.test.js
+++ test/chatroom.test.js
@@ -12,4 +12,12 @@ describe('Chatroom', () => {
     //expect client.username is defined
     expect(client.username).toBeDefined();
   });
+  it('rename new user', () => {
+    const client = {};
+    chatRoom.add(client);
+    const oldName = client.username;
+    expect(chatRoom.rename(client.username, 'new')).toBeTruthy();
+    expect(client.username).toEqual('new');
+    expect(chatRoom.getClient(oldName)).toBeFalsy();
+  });
 });
